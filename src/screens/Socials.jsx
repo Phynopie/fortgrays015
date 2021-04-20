e70e3858c09c78ede7644b67f2442ac1a9fa9f3e
Phynopie <69754690+Phynopie@users.noreply.github.com>
2021-04-20 16:58:53 +0700

    Update Socials.jsx

--- src/screens/Socials.jsx
+++ src/screens/Socials.jsx
@@ -4,8 +4,8 @@ export default function Socials() {
     return (
         <Card title="Connect with me">
             <ul>
-                <li>LinkedIn: <a href="https://www.linkedin.com/in/sagaraaldavy/">Sagara Aldavyk</a></li>
-                <li>Instagram: <a href="https://www.instagram.com/sagaraaldavy/">Sagara Aldavyk</a></li>
+                <li>LinkedIn: <a href="https://www.linkedin.com/in/sagaraaldavy/">Sagara Aldavy</a></li>
+                <li>Instagram: <a href="https://www.instagram.com/sagaraaldavy/">Sagara Aldavy</a></li>
                 <li>Discord: Phynopie#9637</li>
             </ul>
         </Card>
